a62912621f54c10a06a7a002f07e6a428ee15e45
ClaytonPavani <claytonpavani@gmail.com>
2023-09-18 16:05:29 -0300

    adicionando material de herança

---
+++ herança_de_classes.py
@@ -0,0 +1,21 @@
+# Exemplo de como aplicar uma Herança entre classe base e classe filha:
+
+
+# HERANÇA SIMPLES
+'''
+class A:
+    pass
+
+class B(A):
+    pass
+'''
+
+# HERANÇA MULTIPLA
+class A:
+    pass
+
+class B:
+    pass
+
+class C(A, B):
+    pass
